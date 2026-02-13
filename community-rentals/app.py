9ffa74462b748a15f6f4ecfd1b37dd7832ac3eea
alfiagit27 <alfiasset@gmail.com>
2026-02-14 04:10:02 +0530

    render requirements

--- community-rentals/app.py
+++ community-rentals/app.py
@@ -252,4 +252,5 @@ def list_item():
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
